a3481d07e94fa9ea1a35b7d7077fd72df3e6d8be
Rafael Cardoso <rafaelbprates@gmail.com>
2022-10-26 15:01:53 -0300

    [add] challange 15

--- pages/challange-15/js/script.js
+++ pages/challange-15/js/script.js
@@ -1,37 +1,88 @@
 var marcas = [];
 var produtos = [];
+var usuariosRegistrados = [];
 
-function authenticateUser() {
+usuario = {
+  userName: "admin",
+  password: "admin",
+};
+
+usuariosRegistrados.push(usuario);
+localStorage.setItem("usuarios", JSON.stringify(usuariosRegistrados));
+
+function autenticarUsuario() {
+  document.getElementById("btn-login").checkValidity();
+  document.getElementById("btn-login").reportValidity();
+
   var userName = document.getElementById("userName").value;
   var password = document.getElementById("password").value;
 
-  user = {
-    userName: userName,
-    password: password,
-  };
+  let acessoGarantido = false;
+  let usuariosRegistrados = JSON.parse(localStorage.getItem("usuarios"));
 
-  localStorage.setItem("user", user);
+  for (ur of usuariosRegistrados) {
+    if (userName == ur.userName && password == ur.password) {
+      window.location.href =
+        "http://127.0.0.1:5500/pages/challange-15/gerenciar-marcas.html";
+      acessoGarantido = true;
+      break;
+    }
+  }
 
-  console.log(userName);
-  console.log(password);
+  if (!acessoGarantido) {
+    document
+      .getElementById("usuario-nao-encontrado")
+      .classList.remove("invisible");
+    document.getElementById("usuario-nao-encontrado").classList.add("visible");
+    if (localStorage.getItem("tentativasLogin") == null) {
+      localStorage.setItem("tentativasLogin", 1);
+    } else {
+      var tentativasLogin = localStorage.getItem("tentativasLogin");
+      localStorage.setItem("tentativasLogin", ++tentativasLogin);
+    }
+
+    if (tentativasLogin >= 3) {
+      document.getElementById("btn-login").classList.add("disabled");
+      setTimeout(() => {
+        document.getElementById("btn-login").classList.remove("disabled");
+        localStorage.setItem("tentativasLogin", 0);
+      }, 3000);
+    }
+  }
 }
 
-export function cadastrarMarca() {
+function cadastrarMarca() {
+  // carregar o array marcas com os items do localStorage
+  console.log("storageMarcas: " + localStorage.getItem("marcas"));
+  if (localStorage.getItem("marcas") != null) {
+    console.log("entrou aqui!");
+    marcas = JSON.parse(localStorage.getItem("marcas"));
+    console.log(produtos);
+  }
+
+  document.getElementById("btn-cadastrar-marca").checkValidity();
+  document.getElementById("btn-cadastrar-marca").reportValidity();
+
   var nomeMarca = document.getElementById("nomeMarca").value;
 
   if (validarNomeMarca(nomeMarca)) {
-    cadastrar(nomeMarca);
-    // console.log(nomeMarca);
-    // marcas.push({
-    // nomeMarca: nomeMarca,
-    // });
-    // localStorage.setItem("marcas", JSON.stringify(marcas));
+    // cadastrar(nomeMarca);
+    console.log(nomeMarca);
+    marcas.push({
+      nomeMarca: nomeMarca,
+    });
+    localStorage.setItem("marcas", JSON.stringify(marcas));
   }
 
   adicionarMarcaATabela();
 }
 
 function cadastrarProduto() {
+  // carregar o array produtos com os items do localStorage
+  if (localStorage.getItem("produtos") != null) {
+    produtos = JSON.parse(localStorage.getItem("produtos"));
+  }
+
   var nomeProduto = document.getElementById("nomeProduto").value;
   var marcaProduto = document.getElementById("marcaProduto").value;
   var precoProduto = document.getElementById("precoProduto").value;
@@ -52,37 +103,48 @@ function cadastrarProduto() {
 }
 
 function validarNomeMarca(nomeMarca) {
-  let storageMarcas = JSON.parse(localStorage.getItem("marcas"));
-
-  if (nomeMarca.length > 2) {
+  if (nomeMarca.length > 2 && !existeMarca(nomeMarca)) {
     return true;
   }
   return false;
 }
 
 function adicionarMarcaATabela() {
-  var storageMarcas = localStorage.getItem("marcas");
-  storageMarcas = JSON.parse(storageMarcas);
+  if (localStorage.getItem("marcas") != null) {
+    marcas = JSON.parse(localStorage.getItem("marcas"));
+  }
+  // var storageMarcas = localStorage.getItem("marcas");
+  // storageMarcas = JSON.parse(storageMarcas);
 
   var dados = "";
-  for (m of storageMarcas) {
-    dados += `<tr><td>${m.nomeMarca}</td></tr>`;
+  for (m of marcas) {
+    dados += `<tr>
+              <td>${m.nomeMarca}</td>
+              <td><button onclick="excluirMarca(${marcas.indexOf(
+                m
+              )})" class="btn btn-outline-danger btn-sm">Excluir</button></td>
+              </tr>`;
   }
 
   document.getElementsByTagName("tbody")[0].innerHTML = dados;
 }
 
 function adicionarProdutoATabela() {
-  var storageProdutos = localStorage.getItem("produtos");
-  storageProdutos = JSON.parse(storageProdutos);
+  if (localStorage.getItem("produtos") != null) {
+    produtos = JSON.parse(localStorage.getItem("produtos"));
+  }
+  // var storageProdutos = localStorage.getItem("produtos");
+  // storageProdutos = JSON.parse(storageProdutos);
 
   var dados = "";
-  for (p of storageProdutos) {
+  for (p of produtos) {
     dados += `<tr>
               <td>${p.nome}</td>
               <td>${p.marca}</td>
               <td>${p.preco}</td>
-              <td></td>
+              <td><button onclick="excluirProduto(${produtos.indexOf(
+                p
+              )})" class="btn btn-outline-danger btn-sm">Excluir</button></td>
               </tr>`;
   }
 
@@ -100,3 +162,27 @@ function addOpcoesDeMarcas() {
 
   document.getElementById("marcaProduto").innerHTML = opcoes;
 }
+
+function excluirMarca(index) {
+  // Validar se existe produto associado
+  marcas.splice(index, 1);
+  localStorage.setItem("marcas", JSON.stringify(marcas));
+  adicionarMarcaATabela();
+}
+
+function excluirProduto(index) {
+  produtos.splice(index, 1);
+  localStorage.setItem("produtos", JSON.stringify(produtos));
+  adicionarProdutoATabela();
+}
+
+function existeMarca(nome) {
+  for (m of marcas) {
+    if (m.nomeMarca == nome) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function existeProdutoAssociadoMarca(nomeMarca) {}
